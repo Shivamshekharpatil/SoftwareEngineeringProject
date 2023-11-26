ac3646059c893e3f278067f38761cabf70708f88
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2023-11-26 03:46:10 +0000

    Added Data In the User Database

--- SoftwareProject/Program.cs
+++ SoftwareProject/Program.cs
@@ -16,7 +16,7 @@ namespace SoftwareProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Loading());
+            Application.Run(new UserForm1());
         }
     }
 }
