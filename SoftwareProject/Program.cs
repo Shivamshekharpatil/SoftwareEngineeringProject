e65453c28a8e57ea38cdb03455cd48f6007ffb6f
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2023-11-26 03:40:30 +0000

    Added Products and their details also tested the search function if its properly working or not

--- SoftwareProject/Program.cs
+++ SoftwareProject/Program.cs
@@ -16,7 +16,7 @@ namespace SoftwareProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new Loading());
         }
     }
 }
