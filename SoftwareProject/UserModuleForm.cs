32239ab44e3014dcd9e07600cfeba9394e66b6e4
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2023-12-07 17:16:13 +0000

    Comments updated and added in UserModuleForm.

--- SoftwareProject/UserModuleForm.cs
+++ SoftwareProject/UserModuleForm.cs
@@ -11,40 +11,52 @@ using System.Data.SqlClient;
 
 namespace SoftwareProject
 {
+    // The UserModuleForm class represents a form for managing user details.
     public partial class UserModuleForm : Form
     {
+        // SqlConnection object to connect to the database.
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\patil\OneDrive\Documents\dbMS.mdf;Integrated Security=True;Connect Timeout=30");
+        // SqlCommand object for executing SQL queries.
         SqlCommand cm = new SqlCommand();
 
+        // Constructor for the UserModuleForm.
         public UserModuleForm()
         {
+            // Initialize the form components.
             InitializeComponent();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            // Dispose of the form.
             this.Dispose();
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
+                // Check if the passwords match
                 if (txtPass.Text != txtRepass.Text)
                 {
                     MessageBox.Show("Password did not Match!", "Warning",MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
+                // If passwords match, ask for confirmation
                 if (MessageBox.Show("Are you sure want to save this user?", "Saving Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
+                    // Create a SQL INSERT command
                     cm = new SqlCommand("INSERT INTO tbUser(username,fullname,password,phone)VALUES(@username,@fullname,@password,@phone)", con);
                     cm.Parameters.AddWithValue("@username", txtUserName.Text);
                     cm.Parameters.AddWithValue("@fullname", txtFullName.Text);
                     cm.Parameters.AddWithValue("@password", txtPass.Text);
                     cm.Parameters.AddWithValue("@phone", txtPhone.Text);
                     con.Open();
+                    // Execute the INSERT command
                     cm.ExecuteNonQuery();
                     con.Close();
+                    // Display success message
                     MessageBox.Show("User has been successfully saved.");
                     Clear();
                 }
@@ -65,6 +77,7 @@ namespace SoftwareProject
             btnUpdate.Enabled = false;
         }
 
+        // Method to clear text fields
         public void Clear()
         {
             txtUserName.Clear();
@@ -74,25 +87,31 @@ namespace SoftwareProject
             txtPhone.Clear();
         }
 
+        // Event handler for the update button
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             try
             {
+                // Check if the passwords match
                 if (txtPass.Text != txtRepass.Text)
                 {
                     MessageBox.Show("Password did not Match!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
+                // If passwords match, ask for confirmation
                 if (MessageBox.Show("Are you sure want to update this user?", "Update Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
+                    // Create a SQL UPDATE command
                     cm = new SqlCommand("UPDATE tbUser SET username=@username,fullname=@fullname,password=@password,phone=@phone WHERE username LIKE '" + txtUserName.Text + "' ", con);
                     cm.Parameters.AddWithValue("@username", txtUserName.Text);
                     cm.Parameters.AddWithValue("@fullname", txtFullName.Text);
                     cm.Parameters.AddWithValue("@password", txtPass.Text);
                     cm.Parameters.AddWithValue("@phone", txtPhone.Text);
                     con.Open();
+                    // Execute the UPDATE command
                     cm.ExecuteNonQuery();
                     con.Close();
+                    // Display success message
                     MessageBox.Show("User has been successfully Updated.");
                     this.Dispose();
                 }
