cb6e907c02d23af12fd3abd72244146928787ac5
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2023-12-07 17:50:47 +0000

    Comments added to MainForm code.

--- SoftwareProject/MainForm.cs
+++ SoftwareProject/MainForm.cs
@@ -12,39 +12,54 @@ namespace SoftwareProject
 {
     public partial class MainForm : Form
     {
+        // Constructor for the MainForm
         public MainForm()
         {
             InitializeComponent();
         }
 
-        //to show other form in mainform
+        // Method to show other forms within the MainForm
         private Form activeForm = null;
         private void openChildForm(Form childForm)
         {
-            if(activeForm != null)
+            // Close any previously active form
+            if (activeForm != null)
                 activeForm.Close();
+            // Set the new form as the active form
             activeForm = childForm;
+
+            // Set form properties for integration into the panel
             childForm.TopLevel = false;
             childForm.FormBorderStyle = FormBorderStyle.None;
             childForm.Dock = DockStyle.Fill;
+
+            // Add the child form to the panelMain
             panelMain.Controls.Add(childForm);
             panelMain.Tag = childForm;
+
+            // Bring the child form to the front and show it
             childForm.BringToFront();
             childForm.Show();
         }
 
+        // Event handler for the "Users" button click
         private void btnUsers_Click(object sender, EventArgs e)
         {
+            // Open the UserForm1 as a child form
             openChildForm(new UserForm1());
         }
 
+        // Event handler for the "Categories" button click
         private void btnCategories_Click(object sender, EventArgs e)
         {
+            // Open the CategoryForm as a child form
             openChildForm(new CategoryForm());
         }
 
+        // Event handler for the "Product" button click
         private void btnProduct_Click(object sender, EventArgs e)
         {
+            // Open the ProductForm as a child form
             openChildForm(new ProductForm());
         }
 
@@ -53,23 +68,33 @@ namespace SoftwareProject
 
         }
 
- 
 
+        // Event handler for the logout button (pictureBox1) click
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            // Prompt the user for confirmation before logging out
             if (MessageBox.Show("Are you sure want to Logout?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
+                // Create a new instance of the LoginForm
                 LoginForm main = new LoginForm();
+
+                // Hide the current MainForm
                 this.Hide();
+                // Show the LoginForm
                 main.ShowDialog();
             }
             
         }
 
+        // Event handler for the home button (pictureBox2) click
         private void pictureBox2_Click(object sender, EventArgs e)
         {
+            // Create a new instance of the MainForm
             MainForm main = new MainForm();
-            this.Hide(); main.ShowDialog();
+            // Hide the current MainForm
+            this.Hide();
+            // Show the MainForm
+            main.ShowDialog();
         }
 
         private void label7_Click(object sender, EventArgs e)
